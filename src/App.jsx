78068022e5e5fdd29e6110bf99c7b6de7651b1dd
rkhaa25 <mhmdarya098@gmail.com>
2024-07-08 14:34:13 +0700

    membuat web TicTacToe

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { FaRegTimesCircle } from "react-icons/fa";
+import { MdOutlineCircle } from "react-icons/md";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className='square' onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({xIsNext,squares,onPlay}) {
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares))return;
+
+    const nextSquares = squares.slice();
+    
+    nextSquares[i] = xIsNext ? <FaRegTimesCircle /> : <MdOutlineCircle />
+    onPlay(nextSquares);
+  }
+  const winner = calculateWinner(squares);
+  let status = '';
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='status'>{status}</div>
+    <div className='board'>
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    </div>
+    </>
+  );
+}
+
+export default function Game(){
+const [xIsNext,setXIsNext] = useState(true)
+const [history,setHistory] = useState([Array(9).fill(null)])
+const [currentMove,setCurrentMove] = useState(0)
+const currentSquare = history[currentMove];
+//currentSquare adalah aktifitas terakhir
+
+function jumpTo(nextMove){
+  setCurrentMove(nextMove)
+  setXIsNext(nextMove % 2 === 0)
+}
+
+function handlePlay(nextSquares){
+  const nextHistory = [...history.slice(0, currentMove +1), nextSquares]
+  setHistory(nextHistory)
+  setCurrentMove(nextHistory.length -1 )
+    setXIsNext(!xIsNext);
+}
+
+const moves = history.map((squares, move) => {
+  let description = ''
+  if(move > 0 ){
+    description = 'go to move #' + move
+  }else{
+    description = 'go to game start';
+  }
+  return (
+
+    <li key={move}>
+      <button onClick={()=>jumpTo(move)}>{description}</button>
+      </li>
+  )
+} 
+)
+
+  return(
+    <>
+      <div className='game'></div>
+      <div className='game-board'>
+      <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='game-info'>
+        <ol>{moves}</ol>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+function calculateWinner(squares){
+  const lines = [
+    //horizontal
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    //vertikal
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    //diagonal
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i = 0; i < lines.length; i++){
+    const [a,b,c] = lines[i];
+
+  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return false;
+}
+
